9f665c99b14213a784f96f2f007b6bc468f2d72e
kunalthakkar <kunalthakkar99@gmail.com>
2026-04-03 18:45:41 +0530

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,7 +1,10 @@
+const faders = document.querySelectorAll('.fade');
 
-window.addEventListener('scroll',()=>{
- document.querySelectorAll('.fade').forEach(el=>{
-  let pos=el.getBoundingClientRect().top;
-  if(pos<window.innerHeight-100){el.style.opacity=1;el.style.transform='translateY(0)';}
- });
+window.addEventListener('scroll', () => {
+  faders.forEach(el => {
+    const top = el.getBoundingClientRect().top;
+    if (top < window.innerHeight - 100) {
+      el.classList.add('show');
+    }
+  });
 });
